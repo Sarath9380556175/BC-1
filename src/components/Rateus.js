9c0fd186a2766104efb3774232a4ef0c9683b9bf
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-04-06 16:29:56 +0530

    Update Rateus.js

--- src/components/Rateus.js
+++ src/components/Rateus.js
@@ -35,7 +35,7 @@ class Rateus extends React.Component{
     handleSubmit=()=>{
         const {rating,quality,feedback}=this.state;
         axios({
-            url:'http://localhost:8080/rateus',
+            url:'https://fathomless-badlands-79053.herokuapp.com/rateus',
             method:'POST',
             headers:{'content-Type':'application/json'},
             data:
