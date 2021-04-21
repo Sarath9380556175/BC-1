1b954f5f8f46b929ded7d5000e32a78705f4a159
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-04-21 11:34:26 +0530

    Update Quicksearchitem.js

--- src/components/Quicksearchitem.js
+++ src/components/Quicksearchitem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import  '../styles/Home.css';
 import {withRouter} from 'react-router-dom';
+import {motion,AnimateSharedLayout,AnimatePresence} from 'framer-motion';
 class QuicksearchItem extends React.Component{
 
     quicksearch=(mealtypeId)=>{
@@ -15,11 +16,14 @@ class QuicksearchItem extends React.Component{
       <div style={{display: "inline-block",width: "45%"}}>
           <img src={items.image} width="129px" height="149px" alt="notfound" className="rounded-circle"/>
       </div>
+<AnimateSharedLayout>
       <div style={{display: "inline-block",width: "45%",verticalAlign: "top"}} className="ml-3">
-          <div className="badge badge-dark rounded-circle  ml-3">{items.badge}</div>
-          <div className="burger">{items.name}</div>
-          <div className="two">{items.content}</div>
-      </div>
+          <motion.div className="badge badge-dark rounded-circle  ml-3">{items.badge}</motion.div>
+          <motion.div className="burger">{items.name}</motion.div>
+          <motion.div className="two">{items.content}</motion.div>
+
+      </motion.div>
+</AnimateSharedLayout>
 
   </div>
 
