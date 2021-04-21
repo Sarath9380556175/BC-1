4cdfd98b3ce01019a7d043babeb662076aaeb2cd
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-04-21 19:23:34 +0530

    Update Quicksearchitem.js

--- src/components/Quicksearchitem.js
+++ src/components/Quicksearchitem.js
@@ -2,6 +2,13 @@ import React from 'react';
 import  '../styles/Home.css';
 import {withRouter} from 'react-router-dom';
 import {motion,AnimateSharedLayout,AnimatePresence} from 'framer-motion';
+import {easings} from 'react-animation
+
+const mystyles={
+    animation:`pop-in ${easings.easeOutExpo} 5000ms forwards`
+   
+}
+
 class QuicksearchItem extends React.Component{
 
     quicksearch=(mealtypeId)=>{
@@ -12,18 +19,18 @@ class QuicksearchItem extends React.Component{
     {
         const {items}=this.props;
         return(
-  <div className="one rounded"  data-toggle="tooltip" title={items.content} data-placement="bottom" onClick={()=>this.quicksearch(items.badge)}>
+  <motion.div className="one rounded"  data-toggle="tooltip" title={items.content} data-placement="bottom" onClick={()=>this.quicksearch(items.badge)} animate={{rotate:360}}>
       <div style={{display: "inline-block",width: "45%"}}>
           <img src={items.image} width="129px" height="149px" alt="notfound" className="rounded-circle"/>
       </div>
-<AnimateSharedLayout type="crossfade">
+
       <div style={{display: "inline-block",width: "45%",verticalAlign: "top"}} className="ml-3">
-          <motion.div className="badge badge-dark rounded-circle  ml-3">{items.badge}</motion.div>
-          <motion.div className="burger">{items.name}</motion.div>
-          <motion.div className="two">{items.content}</motion.div>
+          <div style={mystyles} className="badge badge-dark rounded-circle  ml-3">{items.badge}</div>
+          <div style={mystyles} className="burger">{items.name}</div>
+          <div style={mystyles} className="two">{items.content}</div>
 
       </motion.div>
-</AnimateSharedLayout>
+
 
   </div>
 
